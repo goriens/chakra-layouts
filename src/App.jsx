df308f6e45c5ce2a47b3484a46a3863a74ba0cea
Sohaib gour <76823175+goriens@users.noreply.github.com>
2022-05-26 03:43:21 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Layout1 } from "./components/Layout1";
+import { Layout2 } from "./components/Layout2";
+import { Layout3 } from "./components/Layout3";
+
+function App() {
+  return (
+    <div className="App">
+      {/*<Layout1 />*/}
+      {/*<Layout2 />*/}
+      <Layout3 />
+    </div>
+  );
+}
+
+export default App;
